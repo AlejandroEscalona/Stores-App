730f8c1be3c0f81b28d803b7e5667e866501dfa0
AlejandroEscalona <1alejandroescalona@gmail.com>
2022-06-28 07:09:53 +0200

    Adapter in progress

--- app/src/main/java/com/example/stores/MainActivity.kt
+++ app/src/main/java/com/example/stores/MainActivity.kt
@@ -2,10 +2,43 @@ package com.example.stores
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.stores.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnClickListener {
+
+    private lateinit var mBinding: ActivityMainBinding
+
+    private lateinit var mAdapter: StoreAdapter
+    private lateinit var mGridLayout: GridLayoutManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        mBinding.btnSave.setOnClickListener {
+            val store = Store(name = mBinding.edName.text.toString().trim())
+            mAdapter.add(store)
+        }
+
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView() {
+        mAdapter = StoreAdapter(mutableListOf(),this)
+        mGridLayout = GridLayoutManager(this,2)
+
+        mBinding.recyclerView.apply {
+            setHasFixedSize(true)
+            layoutManager = mGridLayout
+            adapter = mAdapter
+        }
+    }
+
+
+    //    OnClickListener Interface
+    override fun onClick(store: Store) {
+        TODO("Not yet implemented")
     }
 }
--- app/src/main/java/com/example/stores/StoreAdapter.kt
+++ app/src/main/java/com/example/stores/StoreAdapter.kt
@@ -1,5 +1,8 @@
 package com.example.stores
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -8,19 +11,43 @@ import com.example.stores.databinding.ItemStoreBinding
 class StoreAdapter(private var stores: MutableList<Store>, private var listener: OnClickListener):
         RecyclerView.Adapter<StoreAdapter.ViewHolder>(){
 
+    private lateinit var mContext: Context
+
     inner class ViewHolder(view: View): RecyclerView.ViewHolder(view){
         val binding = ItemStoreBinding.bind(view)
+
+        fun setListener(store: Store){
+            binding.root.setOnClickListener {
+                listener.onClick(store)
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        mContext = parent.context
+
+        val view = LayoutInflater.from(mContext).inflate(R.layout.item_store,parent,false)
+        return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        if(stores.size > 0){
+            val store = stores.get(position)
+
+            with(holder){
+                setListener(store)
+                binding.tvName.text = store.name
+            }
+        }
+
+
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+    override fun getItemCount(): Int = stores.size
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun add(store: Store) {
+        stores.add(store)
+        notifyDataSetChanged()
     }
 }
