301aa6e905ec936bd306844b791487116925d9ab
AlejandroEscalona <1alejandroescalona@gmail.com>
2022-06-29 07:35:07 +0200

    Fragment & interface MainAux

--- app/src/main/java/com/example/stores/EditStoreFragment.kt
+++ app/src/main/java/com/example/stores/EditStoreFragment.kt
@@ -1,20 +1,60 @@
 package com.example.stores
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.example.stores.databinding.FragmentEditStoreBinding
+import com.google.android.material.snackbar.Snackbar
 
 class EditStoreFragment : Fragment() {
 
     private lateinit var mBinding: FragmentEditStoreBinding
+    private var mActivity: MainActivity? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?): View? {
         mBinding = FragmentEditStoreBinding.inflate(inflater,container,false)
         return mBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mActivity = activity as? MainActivity
+
+        mActivity?.supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        mActivity?.supportActionBar?.title = getString(R.string.edit_store_title_fragment)
+
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_save, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            android.R.id.home -> {
+                mActivity?.onBackPressed()
+                true
+            }
+            R.id.action_save -> {
+                Snackbar.make(mBinding.root, getString(R.string.store_added_succes), Snackbar.LENGTH_SHORT).show()
+                true
+            }
+            else -> {
+                return super.onOptionsItemSelected(item)
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        mActivity?.supportActionBar?.setDisplayHomeAsUpEnabled(false)
+        mActivity?.supportActionBar?.title = getString(R.string.app_name)
+        mActivity?.hideFab(true)
+
+        setHasOptionsMenu(false)
+        super.onDestroy()
     }
 
 
--- app/src/main/java/com/example/stores/MainActivity.kt
+++ app/src/main/java/com/example/stores/MainActivity.kt
@@ -7,7 +7,7 @@ import com.example.stores.databinding.ActivityMainBinding
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
 
-class MainActivity : AppCompatActivity(), OnClickListener {
+class MainActivity : AppCompatActivity(), OnClickListener, MainAux {
 
     private lateinit var mBinding: ActivityMainBinding
 
@@ -19,17 +19,33 @@ class MainActivity : AppCompatActivity(), OnClickListener {
         mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
 
-        mBinding.btnSave.setOnClickListener {
-            val store = StoreEntity(name = mBinding.edName.text.toString().trim())
+//        mBinding.btnSave.setOnClickListener {
+//            val store = StoreEntity(name = mBinding.edName.text.toString().trim())
+//
+//            Thread{
+//                StoreApplication.database.storeDao().addStore(store)
+//            }.start()
+//
+//            mAdapter.add(store)
+//        }
 
-            Thread{
-                StoreApplication.database.storeDao().addStore(store)
-            }.start()
-
-            mAdapter.add(store)
+        mBinding.fab.setOnClickListener {
+            launchEditFragment()
         }
 
         setupRecyclerView()
+    }
+
+    private fun launchEditFragment() {
+        val fragment = EditStoreFragment()
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        fragmentTransaction.add(R.id.containerMain, fragment)
+        fragmentTransaction.commit()
+        fragmentTransaction.addToBackStack(null)
+        hideFab()
     }
 
     private fun setupRecyclerView() {
@@ -76,5 +92,10 @@ class MainActivity : AppCompatActivity(), OnClickListener {
                 mAdapter.delete(storeEntity)
             }
         }
+    }
+
+    //MainAux
+    override fun hideFab(isVisible: Boolean) {
+        if(isVisible) mBinding.fab.show() else mBinding.fab.hide()
     }
 }
